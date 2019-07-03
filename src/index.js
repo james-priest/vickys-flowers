39f678591957adc7904dcf97ba438112bb6bcda8
James Priest <james-priest@outlook.com>
2019-07-03 13:24:24 -0700

    feat: Add initial layout and page structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'normalize.css';
+import './styles.css';
+
+// document.getElementById('app').innerHTML = `
+// <h1>Please Hire me!<br>I code good!😜😝</h1>
+// <div>
+//   We use Parcel to bundle this sandbox, you can find more info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+// document.getElementById('app').innerHTML = '<h1>Please Hire me!<br>I code good!😜😝</h1>';
